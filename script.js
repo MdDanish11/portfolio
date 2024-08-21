97e617188e74ae65a79304e13d04438cd1d7b909
Md Danish <165379365+MdDanish11@users.noreply.github.com>
2024-08-21 15:09:27 +0000

    updation

--- script.js
+++ script.js
@@ -38,14 +38,14 @@ $(document).ready(function(){
     // typing text animation script
 
     var typed = new Typed(".typing-1", {
-        strings: ["Full Stack Developer", "Java Developer", "Web Designer"],
+        strings: ["Full Stack Developer", "Web Developer", "Java Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full Stack Developer", "Java Developer", "Web Designer"],
+        strings: ["Full Stack Developer", "Web Developer", "Java Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
